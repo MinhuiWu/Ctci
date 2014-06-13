4f6e16a4584ea68eb34fc9a4afe5858d7e7428bd
MinhuiWu <wuminhui890921@gmail.com>
2014-06-13 09:50:26 -0400

    Update ListNode.java

--- chapter2_LinkedList/ListNode.java
+++ chapter2_LinkedList/ListNode.java
@@ -1,58 +1,68 @@
-
-public class ListNode {
-
-	//implement a single linked list
-	ListNode next = null;
-	int value;
+public class NthToLastNode{
 	
-	public ListNode(int value){
-		this.value = value;
-		this.next = next;
+	/*
+	 * find the kth to last element of a singly linked list
+	 *	
+	 * a)recursive way
+	 * go through the linked list, when hits the end, pass back a counter set to 0	
+	 * each parent calls adds 1 to the counter, until it equals k
+	 * space complexity O(n); time complexity: O(n)	
+	 *	
+	 * b)two pointers, one ahead the other k steps
+	 * in place method use O(1) space, time complexity O(n)
+	 *	
+	 * c)use math method: two scan approach(not good)
+	 * first scan: get the length of the ListNode, kth to last which means length-1-k from the beginning
+	 * in place method use O(1) space, time complexity O(n)		 		
+	 */
+	
+		
+	/* solution one
+	 * use wrapper class as could not pass back a node and a counter using simple return, 
+	 * get the counter a wrapper
+	 */
+	public class CountWrapper{
+		public int counter = 0;
+	}	
+	
+	
+	public static ListNode nthToLastNode_One(ListNode head, int n, CountWrapper count){
+		if(head == null || n <= 0) return null;
+		
+		
+		ListNode current = nthToLastNode_One(head.next, n, count);
+		count.counter = count.counter + 1;
+		if(count.counter == n){
+			return head;
+		}
+		
+		return current;
+		
 	}
 	
 	
 	
-	
-	//get a new List and add new node in the end of the list
-	public void appendToTail(int value){
-		ListNode end = new ListNode(value);
-		ListNode current = this;
-		while(current.next != null){
-			current = current.next; 
-		}
-		current.next=end;
-    }
-	
-	
-	public void printList(ListNode head) {
-        ListNode current = head;
-        System.out.print("{");
-        while (current != null) {
-            if (current.next != null)
-                System.out.print(current.value + ", ");
-            else
-                System.out.println(current.value + "}");
-            current = current.next;
-        }
-    }
-	
-	
-	
-	public static ListNode deleteNode(ListNode head, int value){
-		ListNode current = head;
+	//solution 2
+	public static ListNode nthToLastNode_Two(ListNode head, int n){
+		if(head == null || n <= 0) return null;
 		
-		if(current.value == value){
-			return head.next;
-		}
+		ListNode fast = head;
+		ListNode slow = head;
 		
-		while(current.next != null){
-			if(current.next.value == value){
-				current.next = current.next.next;
-				return head;
+		for(int i = n; i > 0; i--){
+			if(fast != null){
+				fast = fast.next;
+			}else{
+				return null;//list length is smaller than n
 			}
-			current = current.next;
 		}
-		return head;
+		
+		while(fast != null){
+			slow = slow.next;
+			fast = fast.next;
+		}
+		return slow;
+		
 	}
 	
 	
@@ -62,11 +72,14 @@ public class ListNode {
 		head.appendToTail(13);
 		head.appendToTail(10);
 		head.appendToTail(11);
+		head.appendToTail(13);
+		head.appendToTail(10);
+		head.appendToTail(1);
 		
-		head.printList(head);
-		head.deleteNode(head, 13);
-		head.printList(head);
 		
-	}	
-
+		System.out.println(nthToLastNode_Two(head, 4).value);
+		
+		
+	}
+	
 }
