acc7056c845592a41425a230feddf9f4377171c1
MINHUI WU <wuminhui890921@gmail.com>
2014-06-22 16:59:06 -0400

    Create Table.java

---
+++ Chapter8_RestaurantReservation/Table.java
@@ -0,0 +1,11 @@
+public class Table{
+	protect int peopleSize;
+	protect String slot;
+	
+	
+	public Table(int peopleSize, String slot){
+		this.peopleSize = peopleSize;
+		this.slot = slot;
+	}
+	
+}
