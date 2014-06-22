0abbe09aa867b60745555729e854332b13b1ac66
MINHUI WU <wuminhui890921@gmail.com>
2014-06-22 16:59:52 -0400

    Create Reservation.java

---
+++ Chapter8_RestaurantReservation/Reservation.java
@@ -0,0 +1,13 @@
+public interface Reservation{
+	public Table addTable(double time, Costomer cu){		
+	}
+	
+	
+	public Table findEmptyTable(double time, Costomer cu){		
+	}
+	
+	
+	public Table removeTable(double time, Costomer cu){
+		
+	}
+}
