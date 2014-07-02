65c72d80744781c732aee05071696d482a3f212d
MINHUI WU <wuminhui890921@gmail.com>
2014-07-02 16:19:05 -0400

    Update IsBST_4.5.java

--- chapter4_TreeGraph/IsBST_4.5.java
+++ chapter4_TreeGraph/IsBST_4.5.java
@@ -9,18 +9,18 @@ public class IsBST{
 		
 	//in order traversal model(use a static member to record previous value, in order to be remembered during recursion timed)
 	public static int previous = Integer.MIN_VALUE;
-	public boolean isBST(TreeNode root){
-		if(root == null) return true;
-		
+	public boolean isBST(TreeNode root) {
+		if (root == null)  return true;
+		 
 		//left side
-		if(!isBST(root.left)) return false;
+		if (!isBST(root.left))  return false;
 		
 		//current
-		if(root.data <= previous) return false;
+		if (root.data <= previous)  return false;
 		previous = root.data;
 		
 		//right side
-		if(!isBST(root.right)) return false;	
+		if (!isBST(root.right))  return false;	
 		
 		return true;		
 	} 
@@ -28,17 +28,20 @@ public class IsBST{
 	
 	//MAX MIN VALUE method
 	public boolean isBST(TreeNode root){
-		if(root == null) return true;
+		if (root == null)  return true;
 		return isBST(root, Integer.MAX_VALUE, Integer.MIN_VALUE);
 	} 
 		
-	public boolean isBST(TreeNode root, int max, int min){
-		if(root.data > max || root.data < min) return false;
+	public boolean isBSThelper(TreeNode root, int max, int min){
+		if (root.data > max || root.data < min)  return false;
+		 
+		if (root.left.data > root.data)  return false;
 		
-		if(root.left.data > root.data) return false;
-		
-		if(root.right.data < root.data) return false;
-		
+		if (root.right.data < root.data)  return false;
+		  
+		if (isBSThelper(root.left, root.data, min) || isBSThelper(root.right, max, root.data)) {
+			return false;
+		}
 		return true;
 	}
 	
