92b73822899e80cf13ad89d023171845f783e6f0
MINHUI WU <wuminhui890921@gmail.com>
2014-07-13 19:19:54 -0400

    Create Question1_arithmatic.java

---
+++ Chapter18/Question1_arithmatic.java
@@ -0,0 +1,44 @@
+public class Arithmatic{
+    /* not use +, -, *, / to implement arithmatic (add without arithmatic operator) 
+     */
+    public static int add(int a, int b) {
+        if (b == 0)  return a;
+        int sum = a ^ b;
+        int carry = (a & b) << 1;
+        return add(sum, carry);
+        
+    } 
+    
+    /*
+    public static int subtract(int a, int b) {
+        
+    }   
+    */
+    
+    public static int multiply(int a, int b) {
+        int sum = 0;
+        int shiftBit = 1;
+        
+        while ((shiftBit & b) <= a) {
+            if ((shiftBit & b) > 0) {
+                sum = add(a, sum);
+            }
+            shiftBit <<= 1;            
+            a <<= 1;
+        }
+        return sum;    
+    }   
+    
+    /*
+    public static int division(int a, int b) {
+        
+    } 
+    */
+    
+    public static void main(String[] str) {
+        int a = 32;
+        int b = 5;
+        System.out.println(add(a, b));
+        System.out.println(multiply(a, b));
+    }
+}
